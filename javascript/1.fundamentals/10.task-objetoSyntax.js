8b572165662845e2d2722dfdfc6c213e9e3fc1f8
Lucas Cosme <lcds1944@gmail.com>
2023-03-06 05:39:06 -0300

    update learning

---
+++ javascript/1.fundamentals/10.task-objetoSyntax.js
@@ -0,0 +1,19 @@
+/*
+* Object list example 1
+*/
+const lista = {}
+lista.nomeProd = 'Maca'
+lista.preco = 1.80.toFixed(2)
+
+console.log(lista)
+
+/*
+* Example 2
+*/
+
+const lista2 = {
+  nomeProd2: 'Pêra',
+  preco2: 2.80.toFixed(2)
+}
+
+console.log(lista2)
---
+++ javascript/1.fundamentals/9.task-arraysSyntax.js
@@ -0,0 +1,6 @@
+const arrayExample = ['Cachorro', 'Gato']
+console.log(arrayExample[0], arrayExample[1])
+
+console.log('Inserindo mais nomes no Array:')
+arrayExample.push('Peixe', 'Rato')
+console.log(arrayExample)
